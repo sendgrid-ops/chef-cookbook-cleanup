99fbced58f4248d1f0a7ab1aef9cb5fffcc5a80f
Jake Pelletier <ghjnut@gmail.com>
2016-01-29 13:33:52 -0700

    Tabs vs spaces

--- lib/chefclean.rb
+++ lib/chefclean.rb
@@ -9,21 +9,21 @@ module ChefClean
     else
       cookbook_names = ChefClean::KnifeCookbook.list
     end
-		cookbook_names.each { |cookbook_name|
-			puts "COOKBOOK: #{cookbook_name}"
-			cookbook_versions = KnifeCookbook.show cookbook_name
-			puts 'ALL VERSIONS:'
-			puts cookbook_versions
-			revision_lines = separate_revision_lines(cookbook_versions, semantic_delimiter)
-			puts 'BACKED UP VERSIONS:'
-			revision_lines.each { |revision_line|
-				revision_line.shift
-				revision_line.each { |cookbook_version|
-					puts cookbook_version
+    cookbook_names.each { |cookbook_name|
+      puts "COOKBOOK: #{cookbook_name}"
+      cookbook_versions = KnifeCookbook.show cookbook_name
+      puts 'ALL VERSIONS:'
+      puts cookbook_versions
+      revision_lines = separate_revision_lines(cookbook_versions, semantic_delimiter)
+      puts 'BACKED UP VERSIONS:'
+      revision_lines.each { |revision_line|
+        revision_line.shift
+        revision_line.each { |cookbook_version|
+          puts cookbook_version
           KnifeCookbook.backup_cookbook(cookbook_name, cookbook_version, path)
-				}
-			}
-			puts "\n"
+        }
+      }
+      puts "\n"
     }
   end
 
@@ -36,59 +36,59 @@ module ChefClean
     else
       cookbook_names = ChefClean::KnifeCookbook.list
     end
-		cookbook_names.each { |cookbook_name|
-			puts "COOKBOOK: #{cookbook_name}"
-			cookbook_versions = KnifeCookbook.show cookbook_name
-			puts 'VERSIONS:'
-			puts cookbook_versions
-			revision_lines = separate_revision_lines(cookbook_versions, semantic_delimiter)
+    cookbook_names.each { |cookbook_name|
+      puts "COOKBOOK: #{cookbook_name}"
+      cookbook_versions = KnifeCookbook.show cookbook_name
+      puts 'VERSIONS:'
+      puts cookbook_versions
+      revision_lines = separate_revision_lines(cookbook_versions, semantic_delimiter)
       if delete
         puts 'DELETING VERSIONS:'
       else
         puts 'WOULD DELETE:'
       end
-			revision_lines.each { |revision_line|
-				revision_line.shift
-				revision_line.each { |cookbook_version|
-					puts cookbook_version
-					if delete
-						KnifeCookbook.delete(cookbook_name, cookbook_version)
-					end
-				}
-			}
-			puts "\n"
-		}
+      revision_lines.each { |revision_line|
+        revision_line.shift
+        revision_line.each { |cookbook_version|
+          puts cookbook_version
+          if delete
+            KnifeCookbook.delete(cookbook_name, cookbook_version)
+          end
+        }
+      }
+      puts "\n"
+    }
   end
 
 
-	# Given:
-	# [2.1.0, 1.2.0, 1.1.2, 1.1.1, 1.1.0, 0.1.0]
+  # Given:
+  # [2.1.0, 1.2.0, 1.1.2, 1.1.1, 1.1.0, 0.1.0]
   # Major
   # [[2.1.0], [1.2.0, 1.1.2, 1.1.1, 1.1.0], [0.1.0]]
   # Minor
   # [[2.1.0], [1.2.0], [1.1.2, 1.1.1, 1.1.0], [0.1.0]]
-	def ChefClean.separate_revision_lines(cookbook_versions_arr, semantic_delimiter='minor')
-		revision_lines = []
-		cur_revision_line = []
-		last_version = nil
-		for cookbook_version in cookbook_versions_arr do
-			version = Semantic::Version.new cookbook_version
-			if (last_version.nil?)
-				cur_revision_line << version
-			elsif (version.major != last_version.major)
-				revision_lines << cur_revision_line
-				cur_revision_line = []
-				cur_revision_line << version
-			elsif (semantic_delimiter == 'minor' && version.minor != last_version.minor)
-				revision_lines << cur_revision_line
-				cur_revision_line = []
-				cur_revision_line << version
-			else
-				cur_revision_line << version
-			end
+  def ChefClean.separate_revision_lines(cookbook_versions_arr, semantic_delimiter='minor')
+    revision_lines = []
+    cur_revision_line = []
+    last_version = nil
+    for cookbook_version in cookbook_versions_arr do
+      version = Semantic::Version.new cookbook_version
+      if (last_version.nil?)
+        cur_revision_line << version
+      elsif (version.major != last_version.major)
+        revision_lines << cur_revision_line
+        cur_revision_line = []
+        cur_revision_line << version
+      elsif (semantic_delimiter == 'minor' && version.minor != last_version.minor)
+        revision_lines << cur_revision_line
+        cur_revision_line = []
+        cur_revision_line << version
+      else
+        cur_revision_line << version
+      end
       last_version = version
-		end
-		revision_lines << cur_revision_line
-		return revision_lines
-	end
+    end
+    revision_lines << cur_revision_line
+    return revision_lines
+  end
 end
